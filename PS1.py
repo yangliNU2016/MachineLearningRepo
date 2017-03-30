3defce4a8a506f2cb8db6210442ea02aa4134712
yangliNU2016 <yangli2016@u.northwestern.edu>
2017-03-29 21:02:29 -0500

    BFS is still buggy

--- PS1.py
+++ PS1.py
@@ -23,18 +23,31 @@ class Node:
 		'''
 		given a node, will return the value at this node
 		'''
+		return self.value
+
 
 	def get_children(self):
 		
 		'''
 		given a node, will return the children of this node
 		'''
+		return self.children
+		
+	
+		
 
 def breadth_first_search(root):
 	
 	'''
 	given the root node, will complete a breadth-first-search on the tree, returning the value of each node in the correct order
 	'''
+	if root.value != None:
+		print str(root.get_value()) + ' ' 
+	if root.children != None:
+		for key, child in root.children.iteritems():
+			print str(child.get_value()) + ' '
+		for key, child in root.children.iteritems():
+			breadth_first_search(child)
 
 def tester():
 	a = Node()
@@ -44,4 +57,7 @@ def tester():
 	a.children = {1: b}
 	print str(a.get_value()) + ' should be 5.'
 	print str(a.get_children()) + ' should be {1: ' + str(b) + '}.'
-	print str(breadth_first_search(a)) + ' should be 5 7.'
+#	print str(breadth_first_search(a)) + ' should be 5 7.'
+	
+if __name__ == "__main__":
+    tester()
