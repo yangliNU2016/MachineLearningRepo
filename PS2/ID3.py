05d73cc2ac15e738d7e688c8bf068ff1f8dc2135
yangliNU2016 <yangli2016@u.northwestern.edu>
2017-04-13 23:53:16 -0500

    Completed and tested out the dominant node selection

--- PS2/ID3.py
+++ PS2/ID3.py
@@ -12,7 +12,7 @@ def ID3(examples, default):
 	return default
   if ifSameClass(examples):
 	return examples[0].get('Class')
-  eT = calcTargetEntropy(examples, 'Class')
+  buildTree(examples)
   
   
 
@@ -47,27 +47,81 @@ def ifSameClass (examples):
 		return False
   return ret
  
-def calcTargetEntropy(examples, attribute):
+def calcTargetEntropy(examples, attribute, value):
    '''
    Takes an array of examples return the entropy of the target
    '''
    cls = dict()
    total = 0.0
-   for example in examples:
-	total += 1.0
-	if cls.has_key(example.get(attribute)):
-		cls[example.get(attribute)] += 1
-	else:
-		cls[example.get(attribute)] = 1
-		
+   entropy = 0.0
+   
+   if value is None: 
+	for example in examples:
+		total += 1.0
+		if cls.has_key(example.get(attribute)):
+			cls[example.get(attribute)] += 1
+		else:
+			cls[example.get(attribute)] = 1
+   else:
+	for example in examples:
+		if example.get(attribute) == value:
+			total += 1.0
+			if cls.has_key(example.get('Class')):
+				cls[example.get('Class')] += 1
+			else:
+				cls[example.get('Class')] = 1
+
    for cl in cls.keys():
 	cls[cl] = float("{0:.2f}".format(cls[cl] / total))
 
-   entropy = 0.0
    for cl in cls.keys():
 	entropy -= cls[cl] * math.log(cls[cl], 2)
-   
    return entropy
+   
+def buildTree(examples):
+#	rt = Node()
+	eT = calcTargetEntropy(examples, 'Class', None)
+	eTx = dict()
+	
+	atts = {}
+	total = 0.0
+	attNum = 0
+	for attribute in examples[0].keys():
+		if attribute != 'Class':
+			attNum += 1
+			atts[attribute] = {}
+			for example in examples:
+				total += 1.0
+				if atts[attribute].has_key(example.get(attribute)):
+					atts[attribute][example.get(attribute)] += 1
+				else:
+					atts[attribute][example.get(attribute)] = 1
+	total = total / attNum
+	
+	for att in atts:
+		for value in atts.get(att):
+			atts.get(att)[value] /= total
+
+	
+	etp = 0.0		
+	for att in atts:
+		for value in atts.get(att):
+			print calcTargetEntropy(examples, att, value)
+			etp += atts.get(att)[value] * calcTargetEntropy(examples, att, value)
+		eTx[att] = eT - etp
+		etp = 0.0
+	print eTx
+		
+	
+
+	'''
+	for attribute in examples[0]:
+		if attribute != 'Class':
+			
+			calcTargetEntropy(examples, attribute)
+			#eTx[attribute] = eT - calcTargetEntropy(examples, attribute)
+	'''
+	
   
 	
 		
